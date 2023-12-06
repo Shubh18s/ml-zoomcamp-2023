33563fea5278347f77193aea403223e0e08d33ee
Shubhdeep Singh <singh18shubhdeep@gmail.com>
2023-12-06 11:49:59 +0000

    adding week 9 and 10

--- cohorts/2023/09-serverless/my-work/lambda_function.py
+++ cohorts/2023/09-serverless/my-work/lambda_function.py
@@ -9,7 +9,7 @@ interpreter.allocate_tensors()
 input_index = interpreter.get_input_details()[0]['index']
 output_index = interpreter.get_output_details()[0]['index']
 
-
+# url = "https://github.com/alexeygrigorev/clothing-dataset-small/master/test/pants/0dfec862-c49f-430b-a6ef-c7ceb187225e.jpg"
 # data = "clothing-dataset-small/test"
 # item = "pants"
 # url = "c8d21106-bbdb-4e8d-83e4-bf3d14e54c16.jpg"
@@ -27,14 +27,14 @@ classes = ['dress',
  't-shirt']
 
 
-def predict(path):
-    X = preprocessor.from_path(path)
+def predict(url):
+    X = preprocessor.from_url(url)
 
     interpreter.set_tensor(input_index, X)
     interpreter.invoke()
     preds = interpreter.get_tensor(output_index)
     
-    float_predictions = preds[0].to_list()
+    float_predictions = preds[0].tolist()
 
     return dict(zip(classes, float_predictions))
 
